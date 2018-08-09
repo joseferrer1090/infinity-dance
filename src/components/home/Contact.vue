aa6ee43b1555449f34d27b34742279656db63507
Jose Carlos <jcfb90@gmail.com>
2018-08-08 22:26:15 -0500

    Change => final view contact with map

--- src/components/home/Contact.vue
+++ src/components/home/Contact.vue
@@ -110,86 +110,7 @@
                         </form>
                     </div>
                 
-                        
-                        <!--
-						<div id="contact-form-result" data-notify-type="success" data-notify-msg="<i class=icon-ok-sign></i> Message Sent Successfully!"></div>
-						<form class="nobottommargin" id="template-contactform" name="template-contactform" action="include/sendemail.php" method="post">
-							
-			<div class="form-process"></div>
-							<div class="col_one_third">
-								<label for="template-contactform-name">Name <small>*</small></label>
-								<input type="text" id="template-contactform-name" name="template-contactform-name" value="" class="sm-form-control required" />
-							</div>
-
-							<div class="col_one_third">
-								<label for="template-contactform-email">Email <small>*</small></label>
-								<input type="email" id="template-contactform-email" name="template-contactform-email" value="" class="required email sm-form-control" />
-							</div>
-
-							<div class="col_one_third col_last">
-								<label for="template-contactform-phone">Phone</label>
-								<input type="text" id="template-contactform-phone" name="template-contactform-phone" value="" class="sm-form-control" />
-							</div>
-
-							<div class="clear"></div>
-
-							<div class="col_two_third">
-								<label for="template-contactform-subject">Subject <small>*</small></label>
-								<input type="text" id="template-contactform-subject" name="template-contactform-subject" value="" class="required sm-form-control" />
-							</div>
-
-							<div class="col_one_third col_last">
-								<label for="template-contactform-service">Services</label>
-								<select id="template-contactform-service" name="template-contactform-service" class="sm-form-control">
-									<option value="">-- Select One --</option>
-									<option value="Wordpress">Wordpress</option>
-									<option value="PHP / MySQL">PHP / MySQL</option>
-									<option value="HTML5 / CSS3">HTML5 / CSS3</option>
-									<option value="Graphic Design">Graphic Design</option>
-								</select>
-							</div>
-
-							<div class="clear"></div>
-
-							<div class="col_full">
-								<label for="template-contactform-message">Message <small>*</small></label>
-								<textarea class="required sm-form-control" id="template-contactform-message" name="template-contactform-message" rows="6" cols="30"></textarea>
-							</div>
-
-							<div class="col_full hidden">
-								<input type="text" id="template-contactform-botcheck" name="template-contactform-botcheck" value="" class="sm-form-control" />
-							</div>
-
-							<div class="col_full">
-								<button class="button button-3d nomargin" type="submit" id="template-contactform-submit" name="template-contactform-submit" value="submit">Send Message</button>
-							</div>
-
-						</form>
-
-						<script type="text/javascript">
-
-							$("#template-contactform").validate({
-								submitHandler: function(form) {
-									$('.form-process').fadeIn();
-									$(form).ajaxSubmit({
-										target: '#contact-form-result',
-										success: function() {
-											$('.form-process').fadeOut();
-											$(form).find('.sm-form-control').val('');
-											$('#contact-form-result').attr('data-notify-msg', $('#contact-form-result').html()).html('');
-											SEMICOLON.widget.notifications($('#contact-form-result'));
-										}
-									});
-								}
-							});
-
-						</script>
-
-					</div> .postcontent end -->
-
-					<!-- Sidebar
-					============================================= -->
-					<div class="sidebar col_last nobottommargin">
+          <div class="sidebar col_last nobottommargin">
 
 						<address>
 							<strong>Infinity Dance Studio</strong><br>
@@ -227,6 +148,35 @@
 	export default {
 		name: "Contact", 
 		components: {
+		},
+		mounted(){
+				$('#google-map').gMap({
+
+					address: '8901 SW 157th AVE, MIAMI, FLORIDA 33196',
+					maptype: 'ROADMAP',
+					zoom: 14,
+					markers: [
+						{
+							address: "8901 SW 157th AVE, MIAMI, FLORIDA 33196",
+							html: '<div style="width: 300px;"><h4 style="margin-bottom: 8px;">Hi, we\'re <span>Envato</span></h4><p class="nobottommargin">Our mission is to help people to <strong>earn</strong> and to <strong>learn</strong> online. We operate <strong>marketplaces</strong> where hundreds of thousands of people buy and sell digital goods every day, and a network of educational blogs where millions learn <strong>creative skills</strong>.</p></div>',
+							icon: {
+															image: "/_assets/img/map-icon-red.png",
+								iconsize: [32, 39],
+								iconanchor: [13,39]
+							}
+						}
+					],
+					doubleclickzoom: false,
+					controls: {
+						panControl: true,
+						zoomControl: true,
+						mapTypeControl: true,
+						scaleControl: false,
+						streetViewControl: false,
+						overviewMapControl: false
+					}
+
+				});
 
 		}
 	}
